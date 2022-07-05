25831a42d538d522ec14526587f7bf76887c2e41
Manav Kalra <hellomanav>
2022-07-05 12:46:40 +0000

    Initial commit

---
+++ src/main/java/com/manav/slackbot/Application.java
@@ -0,0 +1,9 @@
+package com.manav.slackbot;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}

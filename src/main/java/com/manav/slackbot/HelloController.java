83c866da3db151d2039b02658219557bad5da530
Manav Kalra <hellomanav13@gmail.com>
2022-07-06 15:04:50 +0530

    controller

---
+++ src/main/java/com/manav/slackbot/HelloController.java
@@ -0,0 +1,12 @@
+package com.manav.slackbot;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/hello")
+public class HelloController {
+
+    @Get(uri="/", produces="text/plain")
+    public String index() {
+        return "Example Response";
+    }
+}
